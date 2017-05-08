194f80c409dba535c24f07024bd598de9ac0341b
Zakaria <zakaria.cuet@yahoo.com>
2017-05-09 00:22:23 +1000

    rabbit subscriber

--- app/services/rabbit_subscriber.rb
+++ app/services/rabbit_subscriber.rb
@@ -12,13 +12,7 @@ class RabbitSubscriber
     #exchange.publish(message, :routing_key => routing_key)
     queue.subscribe(:block => true, :manual_ack => false) do |delivery_info, properties, payload|
       puts "Received #{payload}, message properties are #{properties.inspect}"
-      if(queue_name == ENV['BUNNY_BOUNCED_EMAILS_QUEUE'])
-       SentMail.update(payload)
-      elsif(queue_name == ENV['BUNNY_RECIPIENT_RESPONSE_QUEUE'])
-        SentMail.recipient_response(payload)
-      else
-        # do nothing
-      end
+      
     end
     
     conn.stop
