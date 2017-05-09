4932594a2a99e39674cbb60e353b15c078705e1d
Rokan Faruqui <rufaruqui@gmail.com>
2017-05-09 10:50:04 +0600

    account table - refactored

--- db/migrate/20170509043551_create_accounts.rb
+++ db/migrate/20170509043551_create_accounts.rb
@@ -4,7 +4,7 @@ class CreateAccounts < ActiveRecord::Migration[5.0]
       t.string :public_id, :null=>false
       t.string :customer
       t.datetime :active_until
-      t.boolean :recurrent, :default=>0, :null=>false
+      t.boolean :recurrent, :default=>false, :null=>false
       t.string :plan
       t.string :status, :default=>"pending"
       t.string :stripe_charge
