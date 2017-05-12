85bbe2b95c9610caf6e310f59c2b155565d3ed23
Rokan Faruqui <rufaruqui@gmail.com>
2017-05-12 15:35:17 +0600

    plan retrieve - refactored

--- app/services/stripe_plan.rb
+++ app/services/stripe_plan.rb
@@ -41,8 +41,21 @@ class StripePlan
 	    retHash["result"]  =  nil
 
 		begin 
-		@stripe_plan = Stripe::Plan.retrieve(plan_parms[:id])
-		@stripe_plan.update_attributes(plan_parms)
+		  @stripe_plan = Stripe::Plan.retrieve(plan_parms[:id]) 
+	       #update Stripe plan. Stripe allows only individual fields to be updated.
+	        @stripe_plan.name                           =  plan_parms[:name]  		        if plan_parms[:name];  
+            @stripe_plan.metadata[:duration_in_days]	=  plan_parms[:duration_in_days] 	if plan_parms[:duration_in_days]
+            @stripe_plan.metadata[:description	   ]	=  plan_parms[:description]     	if plan_parms[:description]
+            @stripe_plan.metadata[:country 	   ]	    =  plan_parms[:country] 			if plan_parms[:country]
+            @stripe_plan.metadata[:broadcasts 	   ]	=  plan_parms[:broadcasts] 		    if plan_parms[:broadcasts]
+            @stripe_plan.metadata[:credits 	   ]	    =  plan_parms[:credits]			    if plan_parms[:credits]
+            @stripe_plan.metadata[:Display 	   ]	    =  plan_parms[:display]			    if plan_parms[:display]
+            @stripe_plan.metadata[:minimum_members]	    =  plan_parms[:minimum_members]	    if plan_parms[:minimum_members]
+            @stripe_plan.metadata[:maximum_members]	    =  plan_parms[:maximum_members]	    if plan_parms[:maximum_members]
+            @stripe_plan.metadata[:is_plan_for_team]	=  plan_parms[:is_plan_for_team]    if plan_parms[:is_plan_for_team]
+            @stripe_plan.metadata[:is_auto_renewable]	=  plan_parms[:is_auto_renewable]   if plan_parms[:is_auto_renewable]
+
+
 		 retHash["success"] = true if  @stripe_plan.save;
 		@result =  @stripe_plan
 	    retHash["result"] = @result if @result;
