34acec8feb46c333e5c65795054375a7fdc323fe
Rokan Faruqui <rufaruqui@gmail.com>
2017-05-12 11:02:47 +0600

    strip plan create - refactored

--- app/services/stripe_plan.rb
+++ app/services/stripe_plan.rb
@@ -1,43 +1,32 @@
 class StripePlan
 	def self.update(msg)
 		json_msg = JSON.parse(msg, symbolize_names: true)
-		retHash = Hash.new
-	    # return values start
+
+		if(json_msg[:action] == "create")
+			retHash = create_plan(json_msg[:params]);
+		
+		retHash.to_json
+	end
+
+	def create_plan(plan_parms)
+	    retHash = Hash.new 
 	    retHash["success"] = true
 	    retHash["error"]   =  ""
 	    retHash["result"]  =  nil
 		
 		begin 
-		@result = Stripe::Plan.create(json_msg[:params])
-
+		@result = Stripe::Plan.create(plan_parms)
 	    retHash["result"] = @result if @result;
 
 	   rescue Stripe::StripeError => e 
          body = e.json_body
 		 retHash["error"]   = body[:error]
-		  retHash["success"] = false;
+		 retHash["success"] = false;
        rescue => e 
 		 retHash["error"]  = {:message => e.message }
-		  retHash["success"] = false;
+		 retHash["success"] = false;
 	   end  	 
- 
-	    # return values end
-		# email = Email.find_by(public_id: json_msg[:email])
-		# if email.blank?
-		#   retHash["success"] = false
-	 #      retHash["error"]   =  "record not found"
-	 #      retHash["result"]  =  nil
-          
-  #       else
-  #         recipient = email.get_recipient_by_id(json_msg[:contact])
-  #         retHash["success"] = true
-	 #      retHash["error"]   =  false
-	 #      retHash["result"]  =  {email_public_id: email.public_id,email: recipient["emailAddress"],first_name: recipient['firstName'], last_name: recipient['lastName']}
-          
-  #       end
+	   return retHash;
+    end
 
-
-        # finally return this
-		retHash.to_json
-	end
 end
